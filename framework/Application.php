e18768a5b4ffbcb2163c47c711e7372e5e25368d
Alexandr93 <shurik.g93@gmail.com>
2015-09-28 12:06:44 +0300

    Make autoloader

---
+++ framework/Application.php
@@ -0,0 +1,22 @@
+<?php
+namespace Framework;
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 12.09.15
+ * Time: 17:29
+ */
+
+/**
+ * Class Application
+ * главный клас приложения
+ */
+class Application
+{
+    /**
+     * главный метод класа
+     */
+    public function run(){
+
+    }
+}
